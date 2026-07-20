5b6d5b29ad3668dc0a518b3a09eecfb038a68648
Gunanto <gunanto75@gmail.com>
2026-07-20 08:19:31 +0700

    Set public webchat URL for PM2

--- public-app/start-pm2.sh
+++ public-app/start-pm2.sh
@@ -5,4 +5,5 @@ export PORT=3003
 export HOST=0.0.0.0
 export GEZYTECH_API_URL=http://127.0.0.1:3000
 export GEZYTECH_SERVICE_TOKEN=dev-token-shared
+export PUBLIC_WEBCHAT_URL=https://chat.gezytech.web.id/webchat/
 exec /home/pgun/.bun/bin/bun run server/index.ts
